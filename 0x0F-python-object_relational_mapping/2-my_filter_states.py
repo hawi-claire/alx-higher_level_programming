dc63261fbbb400ad3b523f31012b386f30693c6e
hawi-claire <7hawi7@gmail.com>
2024-04-17 13:37:31 -0700

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,20 @@
-#!usr/bin/python3
-"""Module that lists all states from the hbtn_0e_0_usa database."""
-
+#!/usr/bin/python3
+"""Lists all values in the states tables of a database where name
+matches the argument
+"""
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    # Get MySQL credentials and search name from command-line arguments
-    # and # Connect to MySQL server
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
+    cur = db.cursor()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
 
-    # Execute the SQL query to retrieve states with the specified name
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-
-    # Fetch all rows and print the states
-    [print(state) for state in c.fetchall()]
+    for state in states:
+        print(state)
