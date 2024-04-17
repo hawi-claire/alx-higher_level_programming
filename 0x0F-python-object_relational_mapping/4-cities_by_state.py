e1fe43dcb7ddcedffc9e7fc0407c0a6bae5a8d75
hawi-claire <7hawi7@gmail.com>
2024-04-17 13:38:44 -0700

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,17 @@
-#!usr/bin/python3
-"""Module that lists all states from the hbtn_0e_0_usa database."""
-
+#!/usr/bin/python3
+"""List all cities from a database
+"""
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    # Get MySQL credentials and search name from command-line arguments
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    # Connect to MySQL server
-    c = db.cursor()
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id;")
+    states = cur.fetchall()
 
-    # Execute the SQL query to retrieve all states
-    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
-                 FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-
-    # Fetch all rows and print the states
-    [print(city) for city in c.fetchall()]
+    for state in states:
+        print(state)
