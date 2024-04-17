f8023fd7393504ef4c22b27faa9ee5bf914a9e72
hawi-claire <7hawi7@gmail.com>
2024-04-17 14:21:29 -0700

    Add a new state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,30 +1,19 @@
-#!usr/bin/python3
-"""Module that searches for a state in a MySQL database using SQLAlchemy."""
+#!/usr/bin/python3
+"""Function prints a state object with the name
+ passed as argument from the db
+"""
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
 
-if __name__ == "__main__":
-    # Create the SQLAlchemy engine using the provided MySQL credentials
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-
-    # Create a session factory
     Session = sessionmaker(bind=engine)
-
-    # Create a session object
     session = Session()
 
-    # Search for the specified state in the database
-    found = False
-    for state in session.query(State):
-        if state.name == sys.argv[4]:
-            print("{}".format(state.id))
-            found = True
-            break
-
-    # Print a message if the state is not found
-    if found is False:
-        print("Not found")
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    print("Not found" if not state else state.id)
