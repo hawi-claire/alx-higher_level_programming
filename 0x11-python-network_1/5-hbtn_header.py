d2f5d2c6c6ae5689276a807628ff36b6597547a1
hawi-claire <7hawi7@gmail.com>
2024-04-26 14:00:41 -0700

    [Add] Fetches a URL

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,6 @@
-#!usr/bin/python3
-""" Takes in a URL, sends a request to the URL and displays the value
-of the variable X-Request-Idin the response header
+#!/usr/bin/python3
+""" Takes in a URL, sends a request to the URL and displays
+the value of the variable X-Request-Idin the response header
 """
 
 if __name__ == '__main__':
