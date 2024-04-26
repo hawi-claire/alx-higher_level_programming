845a4791d8b1906e54cddc36f9156df86883ad8e
hawi-claire <7hawi7@gmail.com>
2024-04-26 13:25:31 -0700

    [Add] Fetches a URL

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status"""
+"""
+Fetches a URL, sends a request to the URL and displays the value 
+of the X-Request-Id variable found in the header of the response
+"""
 import urllib.request
 
 
