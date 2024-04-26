2192ea84b8070c641d7a8aab86bcd4d6e104fed8
hawi-claire <7hawi7@gmail.com>
2024-04-26 14:11:25 -0700

    [Add] Fetches a URL

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Takes in a URL, sends a request to the URL
+and displays the body of the response.
+"""
+
+
+if __name__ == '__main__':
+    from sys import argv
+    from requests import get
+
+    url = argv[1]
+
+    response = get(url)
+    ERR_TXT = 'Error code: {}'
+    status = response.status_code
+    print(ERR_TXT.format(status) if (status >= 400) else response.text)
