0d0fb7d7311343e3d03a3a4b9f061474c18e3c4e
hawi-claire <7hawi7@gmail.com>
2025-02-21 14:41:55 -0800

    [Add: Files]

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,3 @@
+// Updates the text color of the header element to red
+
+$('header').css('color', '#FF0000');
---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,8 @@
+// Toggles the class of HTML tag 'HEADER' when user clicks the
+// div#toggle_header tag
+
+$('div#toggle_header').click(function () {
+    $('header').ToggleClass('red');
+  });v#toggle_header').click(function () {
+    $('header').ToggleClass('red');
+  });
